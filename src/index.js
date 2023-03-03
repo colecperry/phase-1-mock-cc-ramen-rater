daca5d4236f50cc9df43bdd3daac05b742620748
Cole Perry <colecperry@gmail.com>
2023-03-02 17:08:26 -0800

    updated code

--- src/index.js
+++ src/index.js
@@ -1,93 +1,79 @@
-const getRamen = async () => {
-    const response = await fetch('http://localhost:3000/ramens');
-    const data = await response.json();
-    return data;
-}
+// Fetch ramen data, turn into a json response, set json response equal 
+// to ramens variable, and pass ramens variable into a render function
 
-// function getRamen() {
-//     let data = fetch('http://localhost:3000/ramens')
-//     .then(res => res.json()).catch(err => console.log(err));
-//     console.log(data);
-//     return data;
-// }
+fetch("http://localhost:3000/ramens")
+.then (resp => resp.json())
+.then (ramens => renderRamens(ramens))
 
-function getOneRamen(_id){
-    let data = fetch('http://localhost:3000/ramens/' + _id)
-    .then(res => res.json()).catch(err => console.log(err));
-    console.log(data);
-    return data;
-}
+// Create renderRamens function, pass in ramens and call a for each for "ramen,"
+// for each ramen create an img tag and set equal to image variable
+// set the variable (image)'s source to the json data's response, which equals
+// (ramens), and now (ramen) after the forEach iteration
+// grab div container with ID of #ramen-menu and append new image variable
 
+const renderRamens = (ramens) => {
+    ramens.forEach(ramen => {
+        const image = document.createElement('img')
+        image.src = ramen.image
+        const ramenMenu = document.querySelector('#ramen-menu')
+        ramenMenu.appendChild(image)
 
-const renderOneRamen = async (_id) => {
-    const ramen = await getOneRamen(_id);
-    
-    const ramenDetail = document.getElementById('ramen-detail');
-        const imgElement = ramenDetail.querySelector('img');
-        const ramenName = ramenDetail.querySelector('h2');
-        const ramenRestaurant = ramenDetail.querySelector('h3');
-        imgElement.setAttribute('src', ramen.image);
-        ramenName.textContent = ramen.name;
-        ramenRestaurant.textContent = ramen.restaurant;
+        image.addEventListener('click', () => renderDetails(ramen))
 
-    const ramenRating = document.getElementById('rating-display');
-    const comment = document.getElementById('comment-display');
-
-    ramenRating.textContent = ramen.rating;
-    comment.textContent = ramen.comment;
-}
-
-
-const renderRamen = async () => {
-    const data = await getRamen();
-    const ramenMenu = document.getElementById('ramen-menu');
-    data.forEach(ramen => {
-        const ramenImage = document.createElement('img');
-        ramenImage.setAttribute('src', ramen.image);
-        ramenImage.setAttribute('alt', ramen.name);
-        ramenImage.setAttribute('id', ramen.id);
-        ramenImage.addEventListener("click", () => {
-            renderOneRamen(ramen.id);
-        })
-        // <img src='./assets/....' alt='name..' />
-        ramenMenu.appendChild(ramenImage);
     })
 }
+// Starting above, add a "click" event listener to each image, or for each (Ramen)
+// we are iterating over, and add (renderDetails) as the callback function 
+// Since we are passing in a function renderDetails as the argument instead of 
+// the event we are listening for, we have to use an anonymous function to pass in (Ramen)
+// Set new variable ramenImage, which we grabbed, equal to our json data ramen.image
+// Do the same for name, restaurant, rating, and comment but update innerText
 
-// POST REQUEST FOR ALL FORM INPUT
-// First get the form element by Id and submitBtn element by Id:
-const form = document.getElementById("new-ramen");
+const renderDetails = (ramen) => {
+    const ramenImage = document.querySelector(".detail-image")
+    ramenImage.src = ramen.image
+    const ramenName = document.querySelector(".name")
+    ramenName.innerText = ramen.name
+    const ramenRestaurant = document.querySelector(".restaurant")
+    ramenRestaurant.innerText = ramen.restaurant
+    const ramenRating = document.querySelector("#rating-display")
+    ramenRating.innerText = ramen.rating
+    const ramenComment = document.querySelector("#comment-display")
+    ramenComment.innerText = ramen.comment
 
-// Also get all the form elements except for the input button element
-const formElements = form.querySelectorAll("textarea, input:not(#submit-ramen)")
+}
+//Grab new-ramen form from HTML and add a submit event listener, pass in (handleNewRamen)
+// as the callback function. 
 
-// Create an empty formObj to store all of the form data input
-let formObj = {};
+const newRamenForm = document.querySelector("#new-ramen")
+newRamenForm.addEventListener('submit', handleNewRamen)
 
-// Create onchange handler function which will capture all the form input data as it's being entered
-formElements.forEach((element) => {
-    element.addEventListener("change", (e) => {
-        formObj[e.target.name] = e.target.value;
-    });
-});
+// Create (handleNewRamen) function and pass in the event.
+// Since it is a submit event, add e.PreventDefault and invoke it
+// Grab the "value" for the form input fields in HTML and assign them to new variables
 
-const submitBtn = document.getElementById('submit-ramen');
+function handleNewRamen(e) {
+    e.preventDefault()
+    const newName = document.querySelector("#new-name").value
+    const newRestaurant = document.querySelector("#new-restaurant").value
+    const newImage = document.querySelector("#new-image").value
+    const newRating = document.querySelector("#new-rating").value
+    const newComment = document.querySelector("#new-comment").value
 
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    fetch('http://localhost:3000/ramens', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formObj),
-    }).then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.error(err));
-});
+// Create new function called newRamen and assign it's variables to our form input variables
 
+let newRamen = {
+    name: newName,
+    restaurant: newRestaurant,
+    image: newImage,
+    rating: newRating,
+    comment: newComment
+}
 
+// Call (renderRamens) function and pass in our (newRamen) object
+// Since our (renderRamens) function takes in an array, pass in our (newRamen) object
+// into an array
 
+renderRamens([newRamen])
 
-renderRamen();
-
+}
